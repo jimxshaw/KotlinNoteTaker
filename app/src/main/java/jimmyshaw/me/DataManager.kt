94c03c52da2e9a95829ee4595b1003feb2c23c52
Jim Shaw <jimxshaw@gmail.com>
2019-07-07 17:41:34 -0500

    Add init block and initialize function in DataManager class.

--- app/src/main/java/jimmyshaw/me/DataManager.kt
+++ app/src/main/java/jimmyshaw/me/DataManager.kt
@@ -3,4 +3,22 @@ package jimmyshaw.me
 class DataManager {
   val courses = HashMap<String, CourseInfo>()
   val notes = ArrayList<NoteInfo>()
+
+  init {
+    initializeCourses()
+  }
+
+  private fun initializeCourses() {
+    var course = CourseInfo("android_intents", "Android Programming with Intents")
+    courses.set(course.courseId, course)
+
+    course = CourseInfo(courseId = "android_async", title = "Android Async Programming")
+    courses.set(course.courseId, course)
+
+    course = CourseInfo(title = "Java Language", courseId = "java_lang")
+    courses.set(course.courseId, course)
+
+    course = CourseInfo("java_core", "Java Core Platform")
+    courses.set(course.courseId, course)
+  }
 }
