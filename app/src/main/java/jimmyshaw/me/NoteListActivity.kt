8830c233a374dc3f33f4ab2059e7f174f509e871
Jim Shaw <jimxshaw@gmail.com>
2019-07-10 20:30:56 -0500

    Add new icon vector asset. Add intent to note list activity.

--- app/src/main/java/jimmyshaw/me/NoteListActivity.kt
+++ app/src/main/java/jimmyshaw/me/NoteListActivity.kt
@@ -1,5 +1,6 @@
 package jimmyshaw.me
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +11,14 @@ class NoteListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_note_list)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val activityIntent = Intent(this, MainActivity::class.java)
+
+            startActivity(activityIntent)
         }
     }
 
