7e3f57fb6e134fd6b197b42d9c2938711b215bf1
Jim Shaw <jimxshaw@gmail.com>
2019-07-10 20:14:33 -0500

    Add note list activity.

---
+++ app/src/main/java/jimmyshaw/me/NoteListActivity.kt
@@ -0,0 +1,22 @@
+package jimmyshaw.me
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
