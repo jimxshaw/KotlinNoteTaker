5edc5fa30040604001464f5d141d71decd589d13
Jim Shaw <jimxshaw@gmail.com>
2019-07-25 20:37:07 -0500

    Add NoteRecyclerAdapter class boilerplate.

---
+++ app/src/main/java/jimmyshaw/me/NoteRecyclerAdapter.kt
@@ -0,0 +1,24 @@
+package jimmyshaw.me
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteRecyclerAdapter :
+    RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
