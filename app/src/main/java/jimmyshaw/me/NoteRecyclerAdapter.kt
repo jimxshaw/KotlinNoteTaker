9255d403f5e9478c90d760b0ec99da412d708537
Jim Shaw <jimxshaw@gmail.com>
2019-07-25 21:07:07 -0500

    Implement onBindViewHolder method.

--- app/src/main/java/jimmyshaw/me/NoteRecyclerAdapter.kt
+++ app/src/main/java/jimmyshaw/me/NoteRecyclerAdapter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NoteRecyclerAdapter(private val context: Context) :
+class NoteRecyclerAdapter(private val context: Context, private val notes: List<NoteInfo>) :
     RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
 
     private val layoutInflater = LayoutInflater.from(context)
@@ -23,7 +23,10 @@ class NoteRecyclerAdapter(private val context: Context) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val note = notes[position]
+
+        holder.textCourse?.text = note.course?.title
+        holder.textTitle?.text = note.title
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
