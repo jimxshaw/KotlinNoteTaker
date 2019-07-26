5160874bf492aa326e3b17a3a8b28973e902dfb5
Jim Shaw <jimxshaw@gmail.com>
2019-07-25 20:39:17 -0500

    Add layout inflate to NoteRecyclerAdapter class.

--- app/src/main/java/jimmyshaw/me/NoteRecyclerAdapter.kt
+++ app/src/main/java/jimmyshaw/me/NoteRecyclerAdapter.kt
@@ -1,11 +1,16 @@
 package jimmyshaw.me
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class NoteRecyclerAdapter :
+class NoteRecyclerAdapter(private val context: Context) :
     RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
+
+    private val layoutInflater = LayoutInflater.from(context)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
