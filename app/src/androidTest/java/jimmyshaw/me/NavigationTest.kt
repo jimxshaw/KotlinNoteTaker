ca3e373aa243d2976c72f753e5f9b2e60255f9ab
Jim Shaw <jimxshaw@gmail.com>
2019-08-01 20:38:49 -0500

    Implemented select note after navigation drawer change test method.

--- app/src/androidTest/java/jimmyshaw/me/NavigationTest.kt
+++ app/src/androidTest/java/jimmyshaw/me/NavigationTest.kt
@@ -1,5 +1,7 @@
 package jimmyshaw.me
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import org.junit.Assert.*
@@ -9,6 +11,8 @@ import org.junit.runner.RunWith
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.NavigationViewActions
 import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.jwhh.notekeeper.CourseRecyclerAdapter
 
 @RunWith(AndroidJUnit4::class)
 class NavigationTest {
@@ -18,6 +22,12 @@ class NavigationTest {
 
     @Test
     fun selectNoteAfterNavigationDrawerChange() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_courses))
 
+        val coursePosition = 0
+        onView(withId(R.id.listItems)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<CourseRecyclerAdapter.ViewHolder>(coursePosition, click())
+        )
     }
 }
